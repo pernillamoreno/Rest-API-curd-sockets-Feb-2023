14ad35742c45b2ea9db7be13f483be871b70e6b2
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2023-02-21 00:46:18 +0100

    Update ChattRoomController.java

--- src/main/java/com/example/gertruder/controller/ChattRoomController.java
+++ src/main/java/com/example/gertruder/controller/ChattRoomController.java
@@ -14,77 +14,37 @@ import java.util.List;
 @RequestMapping("/")
 public class ChattRoomController { // CHANGES COMING UP SOOOON!!
     @Autowired
-    private ChattRoomStateSocketHandler chattRoomSocketHandler;
+    private ChattRoomStateSocketHandler chattRoomStateSocketHandler;
     @Autowired
     private ChattRoomService chattRoomService;
 
 
-    @GetMapping("/")
-    public ChattRoom getChattroom(){
-        ChattRoom chattRoom = new ChattRoom();
-        chattRoom.setId(1);
-        chattRoom.setTitel("hej");
-        chattRoom.setAttendeees("");
-        return chattRoom;
-    }
-
-    @RequestMapping("/")
-    public List<ChattRoom>ChattRooms() {
-       List<ChattRoom> chattRooms = new ArrayList<>();
-
-        chattRooms.add(new ChattRoom(1,"bääää","många",true));
-        chattRooms.add(new ChattRoom(2,"bä","bö",false));
-        return chattRooms;
-
-
-        //return ResponseEntity.ok(chattRoomService.getAll());
+    @GetMapping
+    public ResponseEntity<List<ChattRoom>> getChattRoom() {
+        return ResponseEntity.ok(chattRoomService.getChattRoom());
     }
 
 
-   @GetMapping
-    public ResponseEntity<ChattRoom> getStudentById(@PathVariable long chattRoomId) {
-        return ResponseEntity.ok(chattRoomService.get(chattRoomId));
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<ChattRoom>> deleteChattRoom(@PathVariable long id){
+        chattRoomService.delete(id);
+        return getChattRoom();
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/chattRoom")
+    private void addChattRoom(@RequestBody ChattRoom chattRoom) {
+        chattRoomService.addChattRoom(chattRoom);
     }
 
 
 
-   /* @PostMapping("student")
-    @PatchMapping("student")
-    @PutMapping("student")
-    public ResponseEntity<List<Student>> addStudent(@RequestBody Student student) {
-        studentService.save(student);
-        studentStateSocketHandler.broadcast(student.getName() + " was created");
-        return getAllStudents();
+    @PostMapping
+    public ResponseEntity<ChattRoom> createChattRoom(@RequestBody ChattRoom chattRoom) {      
+        
+
+        ChattRoom createChattRoom = chattRoomService.save(chattRoom);
+        chattRoomStateSocketHandler.broadcast(createChattRoom);
+        return ResponseEntity.status(204).body(createChattRoom);
     }
 
-    @DeleteMapping("student")
-    public ResponseEntity<List<Student>> deleteStudent(@RequestBody Student student) {
-        studentService.delete(student.getId());
-        return getAllStudents();
-    }
-
-    @DeleteMapping("student/{studentId}")
-    public ResponseEntity<List<Student>> deleteStudent(@PathVariable long studentId) {
-        studentService.delete(studentId);
-        return getAllStudents();
-    }
-
-    @PatchMapping("student/online/{state}/{studentId}")
-    public void setOnlineState(@PathVariable String state, @PathVariable long studentId) {
-        Student newStudent = studentService.get(studentId);
-        Student oldStudent = newStudent.clone();
-
-        switch(state) {
-            case "online": newStudent.setOnline(true); break;
-            case "offline": newStudent.setOnline(false); break;
-            default: throw new IllegalStateException(state + " was illdefined");
-        }
-
-
-        studentService.save(newStudent);
-
-        // broadcast student changes
-        studentStateSocketHandler.broadcast(oldStudent, newStudent);*/
-    }
-
+}
 
